f93a7cf4b9fd7462cc154bd16e65c47abef62ba5
Matteus Varlesse <varlesse04@gmail.com>
2022-12-28 19:43:50 -0300

    remove verifica-login home-page

--- home-page/verifica-empresa.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-    if($_SESSION['email']) {
-    }
-    else {
-        header('Location: /opt/lampp/htdocs/events4u/home-page/home.php');
-        exit();
-    }
-?>
--- home-page/verifica-usuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if($_SESSION['email']) {
-    }
-    else {
-    header('Location: ../home-page/home.php');
-    exit();
-    }
-?>
